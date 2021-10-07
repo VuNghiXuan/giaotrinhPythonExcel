1991a992a1eb6f20593545374990fbd9194c4672
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-07 21:36:10 +0700

    Add files via upload

---
+++ C1/hello_pyExcel.py
@@ -0,0 +1,12 @@
+# Dấu "#" dùng để ghi chú Python không phiên dịch các dòng này
+" Hoặc bạn cũng có thể ghi chú như thế, chú ý phải có dấu ; cuối câu";
+# Kiểu string (kiểu chữ, ký tự), được đặt trong dấu nháy '', hoặc dấu ""
+# \n : phương thức xuống dòng
+# print : là phương thức, hàm để in ra màn hình Terminal. Ghi chú nhớ viết đúng chữ print và không viết Hoa
+
+"Dưới đây là code để in ra màn hình Teminal";
+print("Hello Word!")
+print('Hello Python Excel!!')
+print("Vũ là tên là tôi.\nUsename là: VuNghiXuan\nĐây là code Python đầu tiên.")
+
+
