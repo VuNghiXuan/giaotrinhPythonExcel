ba518c7a4ddc27746d2571c4d91c77d1b79ba4f0
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-08 08:30:02 +0700

    Add files via upload

---
+++ C2/2.list_In_listPy.py
@@ -0,0 +1,13 @@
+# list và list trong list
+list_DanhSach = ["An", "Cường", "Đào", "Thịnh"]
+list_int = [1, 2, 3, 2.5, 5, 5.6] # Số nguyên (kiểu int) và số thực (kiểu float)
+list_str_num = [ 5, "Hồng", 126.4554]
+
+"Kiểu list trong list, trong bảng Ecxel chứa hàng và cột dữ liệu "
+data_Excel = [["STT", "Họ và tên", "Năm sinh"],
+              [1, "Đặng Vũ Nghi Xuân", 2009],
+              [2, 'Đặng Diệp Quân', 2012]]
+print(list_DanhSach)
+print(list_int)
+print(list_str_num)
+print(data_Excel)
