e271f9bf47bd6714dda9cc21d686325cfbda17e5
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-08 08:29:09 +0700

    Delete Vonglap_For.py

--- C2/Vonglap_For.py
+++
@@ -1,6 +0,0 @@
-# Nhập vào các danh sách học sinh Lớp Python_Excel gồm có 4 tổ 
-hop = ["Bánh", "Kẹo", "Viết", "Cục tẩy"]
-
-# Dùng vòng lặp for lấy tên các loại chứa trong cái hộp
-for element_Hop in hop:
-    print(element_Hop)
