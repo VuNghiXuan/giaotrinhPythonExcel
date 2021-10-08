b4145015396b9483c90c122c0e4106ba9b370816
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-08 16:12:17 +0700

    Add files via upload

---
+++ C3_xlwings/3.Mochualuu.py
@@ -0,0 +1,19 @@
+# import xlwings as xw
+import xlwings as xw
+
+"Comment lại đoạn code xw.Book()";
+# xw.Book() 
+
+"Để thực hiện đoạn code dưới đây";
+xw.Book('Book1')
+
+
+
+# # Từ 1 workbooks trên mở thêm 1 workbooks bằng phương thức
+# xw.books['Book1']
+
+
+
+
+# Từ 1 workbooks trên mở thêm 1 workbooks bằng phương thức
+# xw.books.add()
