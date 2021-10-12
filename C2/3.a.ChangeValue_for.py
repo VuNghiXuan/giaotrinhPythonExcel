d65bdcece558cfe1c2b8c21a85a63ec838b755f8
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-12 14:36:20 +0700

    Add files via upload

---
+++ C2/3.a.ChangeValue_for.py
@@ -0,0 +1,12 @@
+# Nhập vào các danh sách học sinh Lớp Python_Excel gồm có 4 tổ 
+hop = ["Bánh", "Kẹo", "Viết", "Cục tẩy"]
+
+# Số phần tử trong list
+num_index = len(hop)
+
+# Dùng vòng lặp for lấy tên các loại chứa trong cái hộp
+for idx in range(num_index):
+    if hop[idx] == "Kẹo":
+        hop[idx] = "Kim cương"
+        break # break: Nghĩa là, khi tìm thấy điều kiện thì dừng và thoát khỏi vòng lặp
+print(hop)
