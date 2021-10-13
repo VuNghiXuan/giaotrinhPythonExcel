3e133d5cc1060a816e611e5a9943ff77023c43a5
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-13 15:51:21 +0700

    Add files via upload

---
+++ C2/8.class_hocsinh.py
@@ -0,0 +1,28 @@
+class hocsinh():
+
+    # Khởi tạo 1 đối là học sinh
+    def __init__(self, ten, toan, ly, hoa):        
+        self.ten = str(ten)
+        self.toan = float(toan)
+        self.ly = float(ly)
+        self.hoa = float(hoa)
+
+    # Phương thức tính điểm trung bình
+    def diem_TB(self):
+        self.avrg_score = round((self.toan +self.toan + self.ly)/3,3)
+        return self.avrg_score
+
+    # Thuộc tính (Chiều cao, cân nặng)
+    def ketqua(self):
+        if self.avrg_score > 5:
+            return "Đậu"
+        else: return "Rớt"
+
+hs = [hocsinh("Bắc Kiều Phong", 9, 10, 10),
+      hocsinh("Nam Mộ Dung", 5, 6, 7),
+      hocsinh("Nam Đế", 4, 5, 1),
+      hocsinh("Bắc Cái", 10, 0, 1)]
+
+for i_hs in range(len(hs)):
+    print(f'{hs[i_hs].ten}. Có điểm trung bình {hs[i_hs].diem_TB()}. Kết quả: {hs[i_hs].ketqua()}')
+
