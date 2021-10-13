2cb8b7ea4e23058bb1869bc3e884a9298f93acdc
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-13 15:45:57 +0700

    Add files via upload

---
+++ C2/7.class.py
@@ -0,0 +1,11 @@
+class hocsinh():
+
+    def __init__(self, ten):        
+        self.ten = str(ten)
+
+# Cách truyền tham số vào đối tượng
+hs_1 = hocsinh("Nguyễn Văn A")
+
+# Lấy thông tin đối tượng và in ra tên đối tượng
+print(hs_1.ten)
+          
