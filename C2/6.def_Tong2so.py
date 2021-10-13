990c7c8c24b0ef24afad43567489746834aa2274
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-13 09:48:50 +0700

    Add files via upload

---
+++ C2/6.def_Tong2so.py
@@ -0,0 +1,14 @@
+# Xây dựng hàm để tính tổng 2 số 
+def tong2so(a, b):
+    tong = a + b
+    return tong
+
+# Cho a=5, b=2.5
+a = 5
+b = 2.5
+
+# Truyền tham số vào hàm như sau:
+tong_a_b = tong2so(a, b)
+
+# In ra kết quả:
+print("Tổng a + b là: ", tong_a_b)
