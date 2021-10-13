5a1e9684a7d07eca9ae3527274e0d4043c836915
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-13 08:24:21 +0700

    Add files via upload

---
+++ C2/5.VonglapWhile.py
@@ -0,0 +1,7 @@
+# Thời gian bắt đầu
+thoi_gian = 0
+while thoi_gian <10:
+    print(f"Trong phút thứ {thoi_gian+1}, Các em yên lặng")
+    thoi_gian += 1 # thoi_gian tăng lên mỗi phút
+print(f"Các em được nói chuyện kể từ phút thứ {thoi_gian+1}")
+    
