0fce2034621cbfcb6459f365019900a3dfc63eb2
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-13 07:41:17 +0700

    Update 3.a.ChangeValue_for.py

--- C2/3.a.ChangeValue_for.py
+++ C2/3.a.ChangeValue_for.py
@@ -4,7 +4,7 @@ hop = ["Bánh", "Kẹo", "Viết", "Cục tẩy"]
 # Số phần tử trong list
 num_index = len(hop)
 
-# Dùng vòng lặp for lấy tên các loại chứa trong cái hộp
+# Dùng vòng lặp for thay thế giá trị trong list
 for idx in range(num_index):
     if hop[idx] == "Kẹo":
         hop[idx] = "Kim cương"
