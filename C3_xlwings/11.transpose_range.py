191cc8630c168a79587f9bc55603f0b56be268aa
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-14 09:10:09 +0700

    Update 11.transpose_range.py

--- C3_xlwings/11.transpose_range.py
+++ C3_xlwings/11.transpose_range.py
@@ -1,14 +1,30 @@
 import xlwings as xw
 
-# path_input = 'D:\\MyBook_PyExcel\\xlwings\\read_Datas\\movies.xls'
+"Đoạn comment lại"
+# path_input = 'D:\\MyBook_PyExcel\\xlwings\\read_Datas\\1.BangDiem.xlsx'
+
+# # Mở workbook để xem trước dữ liệu
 # xw.Book(path_input)
+"----------"
+
+# Sau khi wb được mở comment 2 dòng trên và thực hiện code sau 
 wb = xw.books.active
-sh1 = wb.sheets('1900s')
-rng1 = sh1.range('A1:D5')
+sh1 = wb.sheets('DS_xeploai')
+rng1 = sh1.range('A1:E5')
 
-# sh_test = wb.sheets.add("test")
-sh_test = wb.sheets("test")
+# Kiểm tra file có tên sheet là "test", nếu không thì tạo sheets("test")
+"Đoạn code trong vòng for này giúp bạn ko bị lỗi khi chạy code nhiều lần với tên sheet 'test' đã tạo trước đó"
+for sh in wb.sheets:
+    if sh.name == "test": 
+        sh_test = sh.name
+        print("Đã tồn tại sheet có tên: 'test'")
+        break
+    else:
+        # Tạo 1 bảng tính có tên là "test"
+        sh_test = wb.sheets.add("test")
+
+# Ghi dữ liệu theo chiều gốc của bảng tính
 sh_test.range("A1").value = rng1.value
-sh_test.range("A7").options(transpose=True).value = rng1.value
 
-print(sh1.name)
+# Đổi chiều dòng thành cột và ngược lại
+sh_test.range("A8").options(transpose=True).value = rng1.value
