f287d6dff103d7dbb1b677a4e419bba306aab24b
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-15 10:28:52 +0700

    Update 17. formatTypeData.py

--- C3_xlwings/17. formatTypeData.py
+++ C3_xlwings/17. formatTypeData.py
@@ -4,14 +4,21 @@ from datetime import datetime as dt
 wb = xw.books.active
 sh = wb.sheets["DS_xeploai"]
 
-
 from datetime import datetime
 
-# Cách ghi 1 list giá trị trong Python
+"Có các kiểu định dạng sau:"
+# sh.range('B1').options(numbers=str).value # Định dạng kiểu String
+# sh.range('C1').options(numbers=int).value # Định dạng interger
+# sh.range('D1').options(numbers=float).value # Định dạng float
+# sh.range('E1').options(dates=dt.date).value # Định dạng kiểu ngày tháng
+
+"Ví dụ định dạng và xử lý kiểu ngày tháng giữa Python và Bảng tính Excel"
 list_day = [['19/08/2020'], ['30/12/2022'], ['30/5/2021'], ['30/11/2021']]
 
 "Ghi định dạng kiểu ngày tháng vào bảng tính";
 sh.range('A11').options(dates=dt.date).value = list_day
+
+
 
 "Định dạng ngày tháng list Python";
 to_day = datetime.strptime('15/10/2021', '%d/%m/%Y')
