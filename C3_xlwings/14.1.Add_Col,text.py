d880c9ab4e9f0d1e03232dc6ed6a5cf6339bb2a0
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-15 07:21:38 +0700

    Update 14.1.Add_Col,text.py

--- C3_xlwings/14.1.Add_Col,text.py
+++ C3_xlwings/14.1.Add_Col,text.py
@@ -8,16 +8,3 @@ sh.range("3:7").insert() # Chèn nhiều dòng
 # sh.range("F:F").insert() # Chèn 1 cột
 sh.range("F:G").insert() # Chèn nhiều cột
 
-
-# "Định dạng so sánh ngày tháng qua đoạn code sau:";
-# from datetime import datetime
-
-# list_day = [['19/08/2020'], ['30/12/2022'], ['30/5/2021'], ['30/11/2021']]
-# sh.range('A5').value =list_day
-
-# to_day = datetime.strptime('14/10/2021', '%d/%m/%Y')
-
-# for i_cell in range(len(list_day)):
-#     rng = datetime.strptime(sh.range(f'A{5 + i_cell}').value, '%d/%m/%Y')
-#     if rng > to_day:
-#         sh.range(f'A{5 + i_cell}').color = (255,0,0)
