effe474fe42fb7a8ef7da8fc29ba05dc6270a056
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-10-17 21:16:28 +0700

    Update VuNghiXuan.py

--- C4_Add_in/VuNghiXuan.py
+++ C4_Add_in/VuNghiXuan.py
@@ -3,17 +3,18 @@ import xlwings as xw
 def main():
     wb = xw.Book.caller()
     sheet = wb.sheets[0]
-    in_str = "" 
-    idx = 0
-    while idx<10:
-        in_str += "oO!Oo"
-        sheet.range(idx+1, idx+1).value = in_str
-        idx += 1
+    list_RowsCols = [[15,21.56,32.98, 4.658],
+                     ['Xuân', 'Lan', 'Thu', 'Cúc'],
+                     ['Mặn', "chằn", "cả", "hai"]]   
+    sheet.range('A1').value = list_RowsCols
+        
     
 @xw.func
 def hello(name):
     return f"Hello {name}!"
 
 if __name__ == "__main__":
+    # set_mock_caller() dễ dàng chuyển qua lại gọi hàm giữa Python và Excel
+    # Mã nguồn khuyến cáo file Excel và mã Python cùng folder.
     xw.Book("VuNghiXuan.xlsm").set_mock_caller()
     main()
