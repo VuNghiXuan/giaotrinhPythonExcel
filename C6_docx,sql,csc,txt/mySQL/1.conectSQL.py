8a34c975e0341d3484140e97bb7e71a82dbda8b2
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-11-21 15:43:55 +0700

    Update 1.conectSQL.py

--- C6_docx,sql,csc,txt/mySQL/1.conectSQL.py
+++ C6_docx,sql,csc,txt/mySQL/1.conectSQL.py
@@ -1,22 +1,16 @@
-import mysql.connector
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Vu05291922"
-)
+import mysql.connector 
 
-# Tạo database
-mycursor =  mydb.cursor()
-"Tạo DataBase mới";
-# createDB.execute("CREATE DATABASE myDataFromPython")
+"""1. Kết nối với MySQL bằng Trình kết nối / Python"""
+# Hàm connect()tạo tạo kết nối đến máy chủ MySQL và trả về một MySQLConnection đối tượng.
+objConectSQl = mysql.connector.connect(user = 'root', password = 'Vu05291922', host = 'localhost') # host='127.0.0.1' tương đương 'localhost'
 
-"Show tất cả database"
-# mycursor.execute("SHOW DATABASES")
-# for cursor in mycursor:
-#   print(cursor)
+"""2. Kết nối class cusor bằng phương thức con trỏ (): chúng được ràng buộc với kết nối trong toàn bộ thời gian tồn tại và tất cả các lệnh được thực thi trong ngữ cảnh của phiên cơ sở dữ liệu được bao bọc bởi kết nối. """
+myCursor = objConectSQl.cursor()
 
-"Đặt biến 1 DB"
-db_Python = 'myDataFromPython'
-print(db_Python)
-
-
+# # Thực thi lệnh bằng execute(), hiển thị liệt kê toàn bộ MySQL
+# myCursor.execute('SHOW DATABASES')
+# for cursor in myCursor:
+#     print(cursor)
+"""3. Tạo 1 DataBase mới"""
+myCursor.execute('CREATE DATABASE TaoRa_DataBase_Moi') # TaoRa_DataBase_Moi: là tên data mới
+objConectSQl.close() # Đóng kết nối
