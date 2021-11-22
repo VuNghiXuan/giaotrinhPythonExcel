8b4f35331fb216a06f76f7f8d8d20aaa83fe5c96
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-11-22 10:57:40 +0700

    Update 12.ghi 1D.py

--- C3_xlwings/12.ghi 1D.py
+++ C3_xlwings/12.ghi 1D.py
@@ -11,18 +11,34 @@ import xlwings as xw
 wb = xw.books.active
 
 # Kiểm tra file có tên sheet là "test", nếu không thì tạo sheets("test")
-"Đoạn code trong vòng for này giúp bạn ko bị lỗi khi chạy code nhiều lần với tên sheet 'test' đã tạo trước đó"
+"Đoạn code trong vòng for này giúp bạn không bị lỗi khi chạy code nhiều lần với tên sheet 'test' đã tạo trước đó"
+# Kiểm tra file có tên sheet là "test", nếu không thì tạo sheets("test")
+"Đoạn code trong vòng for này giúp bạn không bị lỗi khi chạy code nhiều lần với tên sheet 'test' đã tạo trước đó"
+sheetNames = ""
 for sh in wb.sheets:
-    if sh.name == "test": 
-        sh_test = wb.sheets[sh.name]
+    # Nếu tên sheet không phải là "test"
+    if sh.name != "test": 
+        sheetNames += sh.name
+    else: 
+        # Nếu tên sheet là "test"           
+        sheetNames += sh.name
         print("Đã tồn tại sheet có tên: 'test'")
-        break
-    else:
-        # Tạo 1 bảng tính có tên là "test"
-        sh_test = wb.sheets.add("test")
+        break  
 
+# Kiểm tra sự tồn tại sheet test
+if "test" not in sheetNames:
+    sh_test = wb.sheets.add("test") #Tạo sheet("test") mới 
+else: sh_test = wb.sheets("test") # Gán tên sheet("test") 
+ 
 list_1D = [1,2,3]
+datas = ['Nguyễn Văn A', 'Nguyễn Văn B']
 
 # Cách 1: Ghi dữ liệu trên 1 dòng
 sh_test.range("A1").value = list_1D
 sh_test.range("A2").value = list_1D
+
+# Cách 2: Ghi dữ liệu trên 1 dòng
+sh_test.range("A3").value = datas
+
+# Cách 3: Ghi theo vùng dữ liệu (range: nhiều cells) 
+sh_test.range("E1").options(ndim=1).value = sh_test.range('A1:C3').value
