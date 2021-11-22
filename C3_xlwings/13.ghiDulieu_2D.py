33d8820c529ffaf5ed3c30f3cd3978568f0cda36
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-11-22 11:04:13 +0700

    Update 13.ghiDulieu_2D.py

--- C3_xlwings/13.ghiDulieu_2D.py
+++ C3_xlwings/13.ghiDulieu_2D.py
@@ -12,8 +12,6 @@ wb = xw.books.active
 
 # Kiểm tra file có tên sheet là "test", nếu không thì tạo sheets("test")
 "Đoạn code trong vòng for này giúp bạn không bị lỗi khi chạy code nhiều lần với tên sheet 'test' đã tạo trước đó"
-# Kiểm tra file có tên sheet là "test", nếu không thì tạo sheets("test")
-"Đoạn code trong vòng for này giúp bạn không bị lỗi khi chạy code nhiều lần với tên sheet 'test' đã tạo trước đó"
 sheetNames = ""
 for sh in wb.sheets:
     # Nếu tên sheet không phải là "test"
@@ -28,12 +26,11 @@ for sh in wb.sheets:
 # Kiểm tra sự tồn tại sheet test
 if "test" not in sheetNames:
     sh_test = wb.sheets.add("test") #Tạo sheet("test") mới 
-else: sh_test = wb.sheets("test") # Gán tên sheet("test") 
- 
+else: sh_test = wb.sheets("test") # Gán tên sheet("test")  
+
 list_2D = [[4,5],
            [6,7],
            [8,9]]
 
 # Ghi dữ liệu list 2D
 sh_test.range("A2").value = list_2D
-
